8f564a543dd6c49dd8efd9337ec87171e5e19904
Ayke van Laethem <aykevanlaethem@gmail.com>
2017-07-30 17:35:55 +0200

    User input of phone number

--- src/main/java/foundation/privacybydesign/sms/SMSRestApi.java
+++ src/main/java/foundation/privacybydesign/sms/SMSRestApi.java
@@ -1,10 +1,25 @@
 package foundation.privacybydesign.sms;
 
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * REST API for use by the web client.
  */
 @Path("")
 public class SMSRestApi {
+    private static final String OK_RESPONSE = "OK"; // value doesn't really matter
+
+    @POST
+    @Path("send-sms-code")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response sendSmsCode(@FormParam("phone") String phone) {
+        System.out.println("phone number: " + phone);
+        return Response.status(Response.Status.OK).entity
+                (OK_RESPONSE).build();
+    }
 }
---
+++ src/main/webapp/common.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var API_ENDPOINT = '/tomcat/irma_sms_issuer/api/';
+
+function onload() {
+    $('#phone-form').on('submit', onsubmit);
+}
+
+function onsubmit(e) {
+    e.preventDefault();
+    var phone = $('#phone-form input[type=tel]').val().trim();
+    $.post(API_ENDPOINT + 'send-sms-code', {phone: phone})
+        .done(function(e) {
+            console.log('success', e);
+        })
+        .fail(function(e) {
+            console.error('fail', e);
+        });
+}
+
+onload();
