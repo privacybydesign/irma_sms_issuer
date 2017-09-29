5d6a60259f05660fe39017f701c9adf99cf6567f
Ayke van Laethem <aykevanlaethem@gmail.com>
2017-09-29 15:09:08 +0200

    UI: Show minutes/hours when >= 60 seconds for ratelimit

--- src/main/webapp/common.js
+++ src/main/webapp/common.js
@@ -1,19 +1,25 @@
 'use strict';
 
-var API_ENDPOINT = '/tomcat/irma_sms_issuer/api/';
+var API_ENDPOINT = '/irma_sms_issuer/api/';
 
 var MESSAGES = {
     'sending-sms': 'SMS code wordt verstuurd...',
     'sms-sent': 'Bericht verstuurd!',
     'verifying-token': 'Code wordt geverifieerd...',
     'issuing-credential': 'Credential uitgeven...',
+    'second': '%n% seconde',
+    'seconds': '%n% seconden',
+    'minute': '%n% minuut',
+    'minutes': '%n% minuten',
+    'hour': '%n% uur',
+    'hours': '%n% uur',
     'phone-add-success': 'Telefoonnummer toegevoegd.',
     'phone-add-timeout': 'Sessie verlopen. Herlaad de pagina om het opnieuw te proberen.',
     'phone-add-cancel': 'Geannuleerd.',
     'phone-add-error': 'Het is helaas niet gelukt dit telefoonnummer toe te voegen aan de IRMA app.',
     'error:internal': 'Interne fout. Neem contact op met IRMA als dit vaker voorkomt.',
     'error:sending-sms': 'Kan de SMS niet verzenden. Dit is waarschijnlijk een probleem in IRMA. Neem contact op met IRMA als dit vaker voorkomt.',
-    'error:ratelimit': 'Probeer het opnieuw na %seconds% seconden.',
+    'error:ratelimit': 'Probeer het opnieuw over %time%.',
     'error:cannot-validate-token': 'Kan token niet verifieren. Zit er geen typfout in?',
 };
 
@@ -81,8 +87,28 @@ function onSubmitPhone(e) {
             }
             if (errormsg == 'error:ratelimit') {
                 var retryAfter = e.getResponseHeader('Retry-After');
-                // TODO: longer durations (minutes, hours, ...)
-                setStatus('danger', MESSAGES[errormsg].replace('%seconds%', retryAfter));
+                // In JavaScript, we can mostly ignore the fact we're dealing
+                // with a string here and treat it as an integer...
+                if (retryAfter < 60) {
+                    var timemsg = MESSAGES['seconds'];
+                    if (retryAfter == 1) {
+                        var timemsg = MESSAGES['second'];
+                    }
+                } else if (retryAfter < 60*60) {
+                    retryAfter = Math.round(retryAfter / 60);
+                    var timemsg = MESSAGES['minutes'];
+                    if (retryAfter == 1) {
+                        var timemsg = MESSAGES['minute'];
+                    }
+                } else {
+                    retryAfter = Math.round(retryAfter / 60 / 60);
+                    var timemsg = MESSAGES['hours'];
+                    if (retryAfter == 1) {
+                        var timemsg = MESSAGES['hour'];
+                    }
+                }
+                setStatus('danger', MESSAGES[errormsg].replace('%time%',
+                    timemsg.replace('%n%', retryAfter)));
             } else {
                 setStatus('danger', MESSAGES[errormsg]);
             }
