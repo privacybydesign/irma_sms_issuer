03fb908f6977a9beab79031b3f567a9b20fa42ef
bart <b.jacobs@cs.ru.nl>
2017-12-03 15:26:50 +0100

    engelse versie toegevoeg en teksten aangepast

--- src/main/webapp/nl/common.js
+++ src/main/webapp/nl/common.js
@@ -6,7 +6,7 @@ var MESSAGES = {
     'sending-sms': 'SMS code wordt verstuurd...',
     'sms-sent': 'Code is verstuurd! U krijgt een bericht van <strong>%number%</strong>.',
     'verifying-token': 'Code wordt geverifieerd...',
-    'issuing-credential': 'Credential uitgeven...',
+    'issuing-credential': 'Attribuut uitgeven...',
     'second': '%n% seconde',
     'seconds': '%n% seconden',
     'minute': '%n% minuut',
@@ -14,13 +14,13 @@ var MESSAGES = {
     'hour': '%n% uur',
     'hours': '%n% uur',
     'phone-add-success': 'Telefoonnummer toegevoegd.',
-    'phone-add-timeout': 'Sessie verlopen. Herlaad de pagina om het opnieuw te proberen.',
+    'phone-add-timeout': 'De sessie is verlopen. Herlaad de pagina om het opnieuw te proberen.',
     'phone-add-cancel': 'Geannuleerd.',
     'phone-add-error': 'Het is helaas niet gelukt dit telefoonnummer toe te voegen aan de IRMA app.',
     'error:internal': 'Interne fout. Neem contact op met IRMA als dit vaker voorkomt.',
-    'error:sending-sms': 'Kan de SMS niet verzenden. Dit is waarschijnlijk een probleem in IRMA. Neem contact op met IRMA als dit vaker voorkomt.',
+    'error:sending-sms': 'De SMS kan niet verzonden worden. Dit is waarschijnlijk een probleem in IRMA. Neem contact op met IRMA als dit vaker voorkomt.',
     'error:ratelimit': 'Probeer het opnieuw over %time%.',
-    'error:cannot-validate-token': 'Kan code niet verifieren. Zit er geen typfout in?',
+    'error:cannot-validate-token': 'De code niet geverifieerd worden. Zit er geen typfout in?',
 };
 
 // This var is global, as we need it to verify a phone number.
