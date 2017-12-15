f9e38bdaa9fdbc8ba7769c01f4e55677c22eb39e
Ayke van Laethem <aykevanlaethem@gmail.com>
2017-12-15 16:21:33 +0100

    Separate JS and messages for cleaner translation

---
+++ src/main/webapp/en/messages.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var MESSAGES = {
+    'sending-sms': 'SMS message is being sent...',
+    'sms-sent': 'Code has been sent! You will receive a message from number <strong>%number%</strong>.',
+    'verifying-token': 'Code is being verified ...',
+    'issuing-credential': 'Attribute has been issed ...',
+    'second': '%n% seconds',
+    'seconds': '%n% seconds',
+    'minute': '%n% minute',
+    'minutes': '%n% minutes',
+    'hour': '%n% hour',
+    'hours': '%n% hour',
+    'phone-add-success': 'Phone number attribute has been added.',
+    'phone-add-timeout': 'The session timed out. Please reload this page and try again.',
+    'phone-add-cancel': 'Cancelled.',
+    'phone-add-error': 'Adding the phone number attribute failed, unfortunately.',
+    'error:internal': 'Internal error. Please contact IRMA if this happens more often.',
+    'error:sending-sms': 'Sending the SMS message fails. Most likely this is problem in the IRMA system. Please contact IRMA if this happens more often.',
+    'error:ratelimit': 'Please try again in %time%.',
+    'error:cannot-validate-token': 'The code cannot be verified. Is there a typo?',
+};
+
---
+++ src/main/webapp/nl/messages.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var MESSAGES = {
+    'sending-sms': 'SMS code wordt verstuurd...',
+    'sms-sent': 'Code is verstuurd! U krijgt een bericht van <strong>%number%</strong>.',
+    'verifying-token': 'Code wordt geverifieerd...',
+    'issuing-credential': 'Attribuut uitgeven...',
+    'second': '%n% seconde',
+    'seconds': '%n% seconden',
+    'minute': '%n% minuut',
+    'minutes': '%n% minuten',
+    'hour': '%n% uur',
+    'hours': '%n% uur',
+    'phone-add-success': 'Telefoonnummer toegevoegd.',
+    'phone-add-timeout': 'De sessie is verlopen. Herlaad de pagina om het opnieuw te proberen.',
+    'phone-add-cancel': 'Geannuleerd.',
+    'phone-add-error': 'Het is helaas niet gelukt dit telefoonnummer toe te voegen aan de IRMA app.',
+    'error:internal': 'Interne fout. Neem contact op met IRMA als dit vaker voorkomt.',
+    'error:sending-sms': 'De SMS kan niet verzonden worden. Dit is waarschijnlijk een probleem in IRMA. Neem contact op met IRMA als dit vaker voorkomt.',
+    'error:ratelimit': 'Probeer het opnieuw over %time%.',
+    'error:cannot-validate-token': 'De code niet geverifieerd worden. Zit er geen typfout in?',
+};
