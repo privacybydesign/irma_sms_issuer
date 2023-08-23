f005a2db87377c5e4523fac4031bfed312934d40
Bob Hageman <bob@schluss.org>
2023-08-23 11:47:49 +0200

    improve: keep issuer required keys

--- utils/keygen.sh
+++ utils/keygen.sh
@@ -5,6 +5,10 @@ dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 SK=$dir/../build/resources/main/sk
 PK=$dir/../build/resources/main/pk
 
+# Create dir if not exists
+mkdir -p $dir/../build/resources/main
+
+
 if [ ! -e "$SK" ]; then
     # Generate a private key in PEM format
     openssl genrsa -out ${SK}.pem 2048
@@ -14,7 +18,4 @@ if [ ! -e "$SK" ]; then
     openssl rsa -in ${SK}.pem -pubout -outform PEM -out ${PK}.pem
     # Calculate corresponding public key, saved in DER format
     openssl rsa -in ${SK}.pem -pubout -outform DER -out ${PK}.der
-    # Remove leftover files
 fi
-rm -f ${SK}.pem
-rm -f ${PK}.pem
